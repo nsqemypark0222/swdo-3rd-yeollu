408e97dd4d372f84e8dfd2fe300d66d08b405f84
eunchae03 <tete030tete@gmail.com>
2020-04-21 08:51:59 +0900

    상세페이지 commit

--- GeTrend/src/main/java/com/yeollu/getrend/store/controller/ReplyController.java
+++ GeTrend/src/main/java/com/yeollu/getrend/store/controller/ReplyController.java
@@ -31,52 +31,21 @@ public class ReplyController {
 	}
 	
 	//등록
-	@RequestMapping(value = "/replyWrite", method = RequestMethod.GET)
-	@ResponseBody
-	public void replyWrite(InstaReplyVO reply, HttpSession session) {
+	@RequestMapping(value = "/replyWrite", method = RequestMethod.POST)
+	public String replyWrite(InstaReplyVO reply, HttpSession session) {
 		logger.info("{}", reply);
 		String user_email = (String)session.getAttribute("loginemail");
 		logger.info(user_email);
 		reply.setUser_email(user_email);
 		
-		//reply.setStore_no("25523615");
 		int cnt = dao.replyWrite(reply);
 		if(cnt > 0) {
 			logger.info("댓글 등록 성공");
 		} else {
 			logger.info("댓글 등록 실패");
 		}
+		return "redirect:/stores/istoreInfo?store_no=" + reply.getStore_no();
 	}
-	
-	//삭제
-	@RequestMapping(value = "/replyRemove", method = RequestMethod.GET)
-	@ResponseBody
-	public void replyRemove(InstaReplyVO reply, HttpSession session) {
-		logger.info("{}", reply);
-		String user_email = (String)session.getAttribute("loginemail");
-		reply.setUser_email(user_email);
-		int cnt = dao.replyRemove(reply);
-		if(cnt > 0) {
-			logger.info("댓글 삭제 성공");
-		} else {
-			logger.info("댓글 삭제 실패");
-		}	
-	}
-	
-	//리스트
-	@RequestMapping(value = "/replyList", method = RequestMethod.GET)
-	@ResponseBody
-	public ArrayList<HashMap<String,Object>> list(InstaReplyVO reply, HttpSession session) {
 
-		String loginemail = (String)session.getAttribute("loginemail");
-		ArrayList<HashMap<String,Object>> rlist = dao.replyList(reply);
-		for (HashMap<String, Object> hashMap : rlist) {
-			hashMap.put("loginemail", loginemail);
-		}
-		
-		logger.info("replyList {}",rlist);
-		
-		
-		return rlist;
-	}
+	
 }
--- GeTrend/src/main/java/com/yeollu/getrend/store/controller/StoreController.java
+++ GeTrend/src/main/java/com/yeollu/getrend/store/controller/StoreController.java
@@ -1,23 +1,38 @@
 package com.yeollu.getrend.store.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.yeollu.getrend.store.dao.InstaReplyDAO;
+import com.yeollu.getrend.store.dao.LikeDAO;
+import com.yeollu.getrend.store.dao.ScoreDAO;
+import com.yeollu.getrend.store.dao.StoreDAO;
+import com.yeollu.getrend.store.vo.InstaReplyVO;
 import com.yeollu.getrend.store.vo.InstaStoreInfoVO;
+import com.yeollu.getrend.store.vo.LikeVO;
+import com.yeollu.getrend.store.vo.StoreVO;
 
 @Controller
 @RequestMapping(value = "/stores")
 public class StoreController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(StoreController.class);
+	@Autowired
+	private InstaReplyDAO replyDAO;
+	@Autowired
+	private LikeDAO likeDAO;
+	@Autowired
+	private ScoreDAO scoreDAO;
 	
 	@RequestMapping(value = "/istoreInfo", method = RequestMethod.GET)
 	public String istoreInfo(String store_no, HttpSession session, Model model) {
@@ -29,6 +44,50 @@ public class StoreController {
 				break;
 			}
 		}
+            //댓글 리스트
+		    String loginemail = (String)session.getAttribute("loginemail");
+		    InstaReplyVO reply = new InstaReplyVO();
+			reply.setStore_no(store_no);
+			reply.setUser_email(loginemail);
+			ArrayList<HashMap<String,Object>> replyList = replyDAO.replyList(reply);
+			logger.info("replyList {}",replyList);
+			model.addAttribute("replyList", replyList);
+			int replyCount = replyDAO.replyCountByStoreno(store_no);
+			model.addAttribute("replyCount", replyCount);
+			
+			//좋아요 눌렀나
+			LikeVO like =  new LikeVO();
+			like.setStore_no(store_no);
+			like.setUser_email(loginemail);
+			boolean isExistedLike = likeDAO.isExistedLike(like);
+			model.addAttribute("isExistedLike", isExistedLike);
+			
+			//평균 별점
+			int scoreAvg = scoreDAO.scoreAvgByStoreno(store_no);
+			model.addAttribute("scoreAvg", scoreAvg);
 		return "/stores/istoreInfo";
 	}
+	
+
+	@RequestMapping(value="istoreinfo_reply", method=RequestMethod.GET)
+	public String istoreinfo_reply(String store_name, String store_no, Model model) {
+		model.addAttribute("store_name", store_name);
+		model.addAttribute("store_no",store_no);
+		return "stores/istoreInfo_reply";
+	}
+	
+	//댓글삭제
+	@RequestMapping(value = "/deleteReply", method = RequestMethod.GET)
+	public String deleteReply(InstaReplyVO reply) {
+		logger.info("{}", reply);
+		int cnt = replyDAO.replyRemove(reply);
+		if(cnt > 0) {
+			logger.info("댓글 삭제 성공");
+		} else {
+			logger.info("댓글 삭제 실패");
+		}
+		
+		return "redirect:istoreInfo?store_no=" + reply.getStore_no();
+	}
+	
 }
